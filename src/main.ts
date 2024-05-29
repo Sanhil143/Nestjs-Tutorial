816fd8eea3655587d1f09f370abb4cc1a0af230e
Sanhil143 <sanhilrai143@gmail.com>
2024-05-29 23:42:09 +0530

    controller setup with testing

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+@Controller()
+class AppController {
+  @Get()
+  getRootRoute() {
+    return "Hello Smarty";
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(4000);
+}
+
+bootstrap();
