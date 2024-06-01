d50f83cda2a3fe2f9a9c4e433648a457b81b3a38
Sanhil143 <sanhilrai143@gmail.com>
2024-06-02 00:27:23 +0530

    update ervices and repositories

--- nestjs-command-setup/src/message/message.controller.ts
+++ nestjs-command-setup/src/message/message.controller.ts
@@ -1,18 +1,26 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/createMessage.dto';
+import { MessageService } from './message.service';
 
 @Controller('message')
 export class MessageController {
+  messageService: MessageService;
+  constructor() {
+    //use dependency injection when you write development code
+    this.messageService = new MessageService();
+  }
   @Get()
-  listMesages() {}
+  listMesages() {
+    return this.messageService.findAll();
+  }
 
   @Post()
-  createMessage(@Body() body:CreateMessageDto) {
-    console.log(body);
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messageService.createMessage(body.content)
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id:any) {
-    console.log(id);
+  getMessage(@Param('id') id: any) {
+    return this.messageService.findOne(id);
   }
 }
---
+++ nestjs-command-setup/src/message/message.repository.ts
@@ -0,0 +1,25 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessageRepository {
+  async findOne(id: string) {
+    const contents = await readFile('message.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('message.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
+
+  async createMessage(message: string) {
+    const contents = await readFile('message.json', 'utf-8');
+    const messages = JSON.parse(contents);
+    const ids = Object.keys(messages).map((id) => parseInt(id, 10));
+    const highestId = ids.length > 0 ? Math.max(...ids) : 0;
+    const newId = highestId + 1;
+    messages[newId] = { id: newId, content: message };
+    await writeFile('message.json', JSON.stringify(messages));
+  }
+}
