d25adaae8290886e0275dad9d373bc60324f5d43
Sanhil143 <sanhilrai143@gmail.com>
2024-06-04 00:45:27 +0530

    update controller logic

--- mini-project-car-value/src/user/user.controller.ts
+++ mini-project-car-value/src/user/user.controller.ts
@@ -1,12 +1,41 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UserService } from './user.service';
+import { UpdateUserDto } from './dto/updateUser.dto';
 
 @Controller('auth')
 export class UserController {
+  constructor(private userService: UserService) {}
+  
   @Post('signup')
-  createUser(@Body() body:CreateUserDto){
-    console.log(body);
-    
+  async createUser(@Body() body: CreateUserDto) {
+    const user = await this.userService.create(body.email, body.password);
+    if (user.userId) {
+      return { status: true, message: 'user signup successfully' };
+    } else {
+      return { status: false, message: 'error during signup' };
+    }
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id:string){
+    const user = await this.userService.findOne(parseInt(id));
+    return user
+  }
+
+  @Get()
+  findAllUser(){
+    return this.userService.find();
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id:string,@Body() body:UpdateUserDto){
+    return await this.userService.update(parseInt(id),body)
+  }
+
+  @Delete('/:id')
+  async removeUser(@Param('id') id:string){
+    return await this.userService.remove(parseInt(id));
   }
 
 }
--- mini-project-car-value/src/user/user.service.ts
+++ mini-project-car-value/src/user/user.service.ts
@@ -6,8 +6,34 @@ import { User } from '../user/user.entity';
 @Injectable()
 export class UserService {
   constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
-  create(email: string, password: string) {
+
+  async create(email: string, password: string) {
     const user = this.userRepo.create({ email, password });
+    return await this.userRepo.save(user);
+  }
+
+  async findOne(id: number) {
+    return await this.userRepo.findOne({where:{ userId: id }});
+  }
+
+  find() {
+    return this.userRepo.find();
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrs);
     return this.userRepo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.userRepo.remove(user);
   }
 }
